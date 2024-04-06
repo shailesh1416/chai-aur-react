22cd05943045cbdf5940ec68fa682587b5e71e3c
shailesh1416 <shaileshyadav1416@gmail.com>
2024-04-06 09:21:02 +0530

    added redux project- todo list

---
+++ 11ReduxToolKit/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+// reducers, useSelector, useDipatch
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl'>Learn about redux toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
